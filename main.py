35ff0dcf18952d44be22c968655bfe2a2b25c854
yang yizhou <yangyizhou99@gmail.com>
2021-07-14 11:33:16 +0200

    fix: multy configuration

--- main.py
+++ main.py
@@ -4,13 +4,23 @@ from src import csv_read
 from src import DecisionTree
 from matplotlib import pyplot as plt
 from sklearn import tree
+from sklearn.model_selection import GridSearchCV
 if __name__ == '__main__':
     input_train, input_test, target_train, target_test = csv_read.load_data()
-    model=DecisionTree(max_depth=2, random_state=42)
-    model.fit(input_train,target_train)
-    output=model.predict(input_test)
-    save = pd.DataFrame({'output': output})
+    param_grid = [
+        {'min_samples_split': [3, 10, 30], 'min_samples_leaf': [2, 4, 6, 8]}
+    ]
+    model = DecisionTree(random_state=42)
+    grid_search = GridSearchCV(model, param_grid, cv=5,
+                               scoring='neg_mean_squared_error',
+                               return_train_score=True)
+    grid_search.fit(input_train, target_train)
+    cvres = grid_search.cv_results_
+    # model.fit(input_train,target_train)
+    # output=model.predict(input_test)
+    save = pd.DataFrame({'score': cvres["mean_test_score"],
+                         'parameters':cvres["params"]})
     save.to_csv('result.csv', index=False)
     fig = plt.figure(figsize=(25, 20))
-    _ = tree.plot_tree(model)
+    _ = tree.plot_tree(grid_search.best_estimator_)
     fig.savefig("decistion_tree.png")
--- src/decision_tree.py
+++ src/decision_tree.py
@@ -8,11 +8,34 @@ class DecisionTree(DecisionTreeRegressor):
     '''
         this class is decision tree
     '''
-    def __init__(self, **kwargs):
+    def __init__(self, *,
+                 criterion="mse",
+                 splitter="best",
+                 max_depth=None,
+                 min_samples_split=2,
+                 min_samples_leaf=1,
+                 min_weight_fraction_leaf=0.,
+                 max_features=None,
+                 random_state=None,
+                 max_leaf_nodes=None,
+                 min_impurity_decrease=0.,
+                 min_impurity_split=None,
+                 ccp_alpha=0.0):
         '''
            this method initialize
         '''
-        super().__init__(**kwargs)
+        super().__init__(criterion=criterion,
+                 splitter=splitter,
+                 max_depth=max_depth,
+                 min_samples_split=min_samples_split,
+                 min_samples_leaf=min_samples_leaf,
+                 min_weight_fraction_leaf=min_weight_fraction_leaf,
+                 max_features=max_features,
+                 random_state=random_state,
+                 max_leaf_nodes=max_leaf_nodes,
+                 min_impurity_decrease=min_impurity_decrease,
+                 min_impurity_split=min_impurity_split,
+                 ccp_alpha=ccp_alpha)
         self.nothing=0
 
     def do_right(self):
